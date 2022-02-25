1c2ddf59ef64c5bd2c0fb42a02bbb975e81fdde2
Hevin Jant <hevin.jant@gmail.com>
2022-02-25 00:48:40 -0800

    raycast bug, only detect table 6

--- abrewtifulday/Assets/Scripts/PutDrinks.cs
+++ abrewtifulday/Assets/Scripts/PutDrinks.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PutDrinks : MonoBehaviour
 {
+    [SerializeField] private GameObject hand;
+    [SerializeField] private LayerMask pickupLayer;
+    [SerializeField] private float pickupRange;
+    [SerializeField] private GameObject trayCoffee; 
+    [SerializeField] private GameObject tableCoffee;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        tableCoffee.active = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.E)) {
+            Debug.Log("E");
+            Ray putRay = new Ray(hand.transform.position, hand.transform.forward);
+
+            if (Physics.Raycast(putRay, out RaycastHit hitInfo, pickupRange, pickupLayer)) {
+                Debug.Log("Found a table");
+                // if we have coffee to serve
+                if (trayCoffee.active) {
+                    trayCoffee.active = false;
+                    tableCoffee.active = true;
+                }
+            }
+        }
     }
 }
