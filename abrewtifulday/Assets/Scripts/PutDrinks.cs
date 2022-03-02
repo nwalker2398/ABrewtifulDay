e47ee9f57bcb97c0a7949e8d99b57c14ce639306
Hevin Jant <hevin.jant@gmail.com>
2022-03-01 16:56:30 -0800

    serve coffee directly to customers fixed

--- abrewtifulday/Assets/Scripts/PutDrinks.cs
+++ abrewtifulday/Assets/Scripts/PutDrinks.cs
@@ -3,28 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /*
-Work done to put coffee on the table:
-- put coffee object on every table.
+Work done to serve coffee to customers:
+- put coffee object on every customer object.
 - raycast from the coffee tray.
-- Box Collider with IsTrigger on for each table. (Collider is a must for raycast)
-- assign Put layer to all tables.
-- PutDrinks script for each table.
-- 
+- Box Collider for each customer object. (Collider is a must for raycast)
+- assign Put layer to customer object.
+- PutDrinks script for each customer.
 */
 
 public class PutDrinks : MonoBehaviour
 {
     [SerializeField] private GameObject hand; // in this case the hand is the coffee tray.
     [SerializeField] private LayerMask pickupLayer;
-    [SerializeField] private float pickupRange = 10;
+    [SerializeField] private float pickupRange;
     [SerializeField] private GameObject trayCoffee; 
-    [SerializeField] private GameObject tableCoffee;
-    [SerializeField] private float serveRadius = 2; 
+    [SerializeField] private GameObject objectCoffee;
 
-    // Start is called before the first frame update
     void Start()
     {
-        tableCoffee.active = false;
+        objectCoffee.active = false;
     }
 
     void OnMouseDown() {
@@ -33,33 +30,18 @@ public class PutDrinks : MonoBehaviour
         Ray putRay = new Ray(hand.transform.position, hand.transform.forward);
 
         if (Physics.Raycast(putRay, out RaycastHit hitInfo, pickupRange, pickupLayer)) {
-            Debug.Log("Raycast hit:" + hitInfo.transform.gameObject.name);
-            // if we have coffee to serve, put the coffee onto the table
-            //if (trayCoffee.active) {
-            if (trayCoffee.active && tableCoffee.transform.parent.name == hitInfo.transform.gameObject.name) {
+            // if we have coffee to serve, put the coffee onto the object
+            if (trayCoffee.active && objectCoffee.transform.parent.name == hitInfo.transform.gameObject.name) {
+                Debug.Log("Object: " + objectCoffee.transform.parent.name + ", Hit Info: " + hitInfo.transform.gameObject.name);
                 trayCoffee.active = false;
-                tableCoffee.active = true;
+                objectCoffee.active = true;
                 ScoreSystem.incrementScore(1);
-
-                Collider[] hitColliders = Physics.OverlapSphere(tableCoffee.transform.position, serveRadius);
-                foreach (var hitCollider in hitColliders)
-                {
-                   if (hitCollider.ToString().Contains("ustomer"))
-                    {
-                        Debug.Log("customer");
-                        Debug.Log(hitCollider.gameObject.GetComponent<Customer>());
-                        Customer c = hitCollider.gameObject.GetComponent<Customer>();
-                        c.Drink(tableCoffee.transform.position, tableCoffee);
-                        break;
-                        //((Customer)hitCollider.gameObject).Drink(tableCoffee.transform.position);
-                    }
-                }
             }
-            // pick the coffee from the table
-            else if (trayCoffee.active == false && tableCoffee.active == true) {
-                trayCoffee.active = true;
-                tableCoffee.active = false;
-            }
+            // pick the coffee from the object
+            // else if (trayCoffee.active == false && objectCoffee.active == true) {
+            //     trayCoffee.active = true;
+            //     objectCoffee.active = false;
+            // }
         }
     }
 }
