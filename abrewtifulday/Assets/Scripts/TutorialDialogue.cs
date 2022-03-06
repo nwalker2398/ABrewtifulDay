17541c0ddbcd5b8b7e869882446d60116994fc06
Katie Seidl <kseidl@calpoly.edu>
2022-03-06 14:41:08 -0800

    added tutorial scene and started making dialogue too

---
+++ abrewtifulday/Assets/Scripts/TutorialDialogue.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialDialogue : MonoBehaviour
+{
+    string SeatingDialog = "Click the customer and then click a chair to seat them!";
+    string makingCoffeeDialog = "Walk to the coffee machine and click it to make a coffee, and once it is done brewing click again to pick it up.";
+    string serveCoffeeDialog = "Walk to the customer and click on them to give them their coffee.";
+    string happyBarDialog = "When you make a customer happy, your happiness quota will become more filled!";
+  
+    //public TMP_InputField tMP_Input;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //tMP_Input = gameObject.GetComponent<TMP_InputField>();
+        //tMP_Input.text = SeatingDialog;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
